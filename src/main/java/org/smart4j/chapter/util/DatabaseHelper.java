c2a800a0aa3d7da9e691a7173dc7a98e8726a934
lazyTimes <1097483508@qq.com>
2019-09-07 20:47:27 +0800

    工具类功能完善

--- src/main/java/org/smart4j/chapter/util/DatabaseHelper.java
+++ src/main/java/org/smart4j/chapter/util/DatabaseHelper.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -57,11 +58,12 @@ public class DatabaseHelper {
 
     /**
      * 使用threadLocal 改写的数据库链接
+     *
      * @return
      */
-    public static Connection getConnection(){
+    public static Connection getConnection() {
         Connection connection = CONNECTION_THREAD_LOCAL.get();
-        if(connection == null){
+        if (connection == null) {
             try {
                 connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             } catch (SQLException e) {
@@ -78,18 +80,19 @@ public class DatabaseHelper {
      * 查询的第二种方式，使用内置connection 的threadlocal进行操作
      * 查询数据列表， 使用dbutil 进行查询
      * BeanListHandler 处理bean 结果
+     *
      * @param entityClass 实体对象class
-     * @param sql 查询语句
-     * @param params 查询参数
-     * @param <T> 泛型参数
+     * @param sql         查询语句
+     * @param params      查询参数
+     * @param <T>         泛型参数
      * @return
      */
-    public static <T> List<T> queryEntityList(Class<T> entityClass, String sql, Object... params){
+    public static <T> List<T> queryEntityList(Class<T> entityClass, String sql, Object... params) {
         List<T> entityList = null;
         Connection connection = getConnection();
         try {
             entityList = QUERY_RUNNER.query(connection, sql, new BeanListHandler<T>(entityClass), params);
-        } catch(SQLException e){
+        } catch (SQLException e) {
             LOGGER.error("query entity failure", e);
             throw new RuntimeException(e);
         } finally {
@@ -100,16 +103,17 @@ public class DatabaseHelper {
 
     /**
      * 用于复杂查询使用，Map列名与列值成对应关系
-     * @param sql 查询语句
+     *
+     * @param sql    查询语句
      * @param params 参数
-     * @return List<Map<String, Object>> 键值对对象集合
+     * @return List<Map < String, Object>> 键值对对象集合
      */
-    public static List<Map<String, Object>> executeQuery(String sql, Object... params){
+    public static List<Map<String, Object>> executeQuery(String sql, Object... params) {
         List<Map<String, Object>> result;
         try {
             Connection connection = getConnection();
             result = QUERY_RUNNER.query(connection, sql, new MapListHandler(), params);
-        }catch (Exception e){
+        } catch (Exception e) {
             LOGGER.error("execute query failure", e);
             throw new RuntimeException(e);
         }
@@ -119,18 +123,19 @@ public class DatabaseHelper {
 
     /**
      * 唯一查询，根据主键查找实体类
+     *
      * @param entityClass 实体类
-     * @param sql 数据库链接语句
-     * @param params 参数
+     * @param sql         数据库链接语句
+     * @param params      参数
      * @param <T>
      * @return
      */
-    public static <T> T queryEntity(Class<T> entityClass, String sql, Object... params){
+    public static <T> T queryEntity(Class<T> entityClass, String sql, Object... params) {
         T entity = null;
         try {
             Connection connection = getConnection();
             entity = QUERY_RUNNER.query(connection, sql, new BeanHandler<T>(entityClass), params);
-        } catch(SQLException e){
+        } catch (SQLException e) {
             LOGGER.error("query entity failure", e);
             throw new RuntimeException(e);
         } finally {
@@ -139,24 +144,126 @@ public class DatabaseHelper {
         return entity;
     }
 
+    /**
+     * 更新记录
+     *
+     * @param params 请求参数
+     * @param sql    查询语句
+     * @return
+     */
+    public static int executeUpdate(String sql, Object... params) {
+        int rows = 0;
+        Connection connection = getConnection();
+        try {
+            rows = QUERY_RUNNER.update(connection, sql, params);
+        } catch (SQLException e) {
+            LOGGER.error("update failure", e);
+            throw new RuntimeException();
+        }
+        return rows;
+    }
+
+    /**
+     * 插入实体
+     *
+     * @param entityClass 实体类
+     * @param filedMap    参数
+     * @return
+     */
+    public static <T> boolean insertEntity(Class<T> entityClass, Map<String, Object> filedMap) {
+        if (CollectionUtil.isEmpty(filedMap)) {
+            LOGGER.error("can not insert entity: filedMap is empty");
+            return false;
+        }
+
+        String sql = "INSERT INTO" + getTableName(entityClass);
+        StringBuilder columns = new StringBuilder("(");
+        StringBuilder values = new StringBuilder("(");
+        for (String key : filedMap.keySet()) {
+            columns.append(key).append(",");
+            values.append("?,");
+        }
+        // 去掉最尾部的逗号
+        columns.replace(columns.lastIndexOf(","), columns.length(), ")");
+        values.replace(values.lastIndexOf(","), values.length(), ")");
+        sql += columns + " VALUES " + values;
+
+        Object[] params = filedMap.values().toArray();
+        return executeUpdate(sql, params) == 1;
+    }
+
+    /**
+     * 更新实体类
+     * @param entityClass
+     * @param id
+     * @param filedMap
+     * @param <T>
+     * @return
+     */
+    public static <T> boolean updateEntity(Class<T> entityClass, int id, Map<String, Object> filedMap) {
+        // TODO : 注意参数的间隙
+        if (CollectionUtil.isEmpty(filedMap)) {
+            LOGGER.error("can not insert entity: filedMap is empty");
+            return false;
+        }
+
+        String sql = "UPDATE " + getTableName(entityClass) + " SET ";
+        StringBuilder columns = new StringBuilder();
+        for (String filedName : filedMap.keySet()) {
+            columns.append(filedName).append("=?, ");
+        }
+        sql += columns.substring(0, columns.lastIndexOf(", ")) + " WHERE id=?";
+        // 拼接更新参数
+        List<Object> paramList = new ArrayList<Object>();
+        paramList.addAll(filedMap.values());
+        paramList.add(id);
+        Object[] params = paramList.toArray();
+
+        return executeUpdate(sql, params) == 1;
+
+    }
+
+    /**
+     * 删除实体
+     * @param entityClass 实体类
+     * @param id 主键
+     * @param <T>
+     * @return
+     */
+    public static <T> boolean deleteEntity(Class<T> entityClass, int id){
+        String sql = "DELETE FROM " + getTableName(entityClass) + " WHERE id=?";
+        return executeUpdate(sql, id) == 1;
+    }
+
+    /**
+     * 获取表名
+     *
+     * @param entityClass
+     * @return
+     */
+    private static <T> String getTableName(Class<T> entityClass) {
+        return entityClass.getSimpleName();
+    }
+
 
     /**
      * 此方式不建议使用
      * 查询数据列表， 使用dbutil 进行查询
      * BeanListHandler 处理bean 结果
-     * @param connection 数据库链接
+     *
+     * @param connection  数据库链接
      * @param entityClass 实体对象class
-     * @param sql 查询语句
-     * @param params 查询参数
-     * @param <T> 泛型参数
+     * @param sql         查询语句
+     * @param params      查询参数
+     * @param <T>         泛型参数
      * @return
      */
     @Deprecated
-    public static <T> List<T> queryEntityList(Connection connection, Class<T> entityClass, String sql, Object... params){
+    public static <T> List<T> queryEntityList(Connection connection, Class<T> entityClass, String sql, Object... params) {
         List<T> entityList = null;
         try {
             entityList = QUERY_RUNNER.query(connection, sql, new BeanListHandler<T>(entityClass), params);
-        } catch(SQLException e){
+        } catch (SQLException e) {
             LOGGER.error("query entity failure", e);
             throw new RuntimeException(e);
         } finally {
@@ -165,6 +272,8 @@ public class DatabaseHelper {
         return entityList;
 
     }
+
+
 
     /*
      * 获取数据库连接
@@ -185,9 +294,9 @@ public class DatabaseHelper {
     /**
      * 使用threadlocal 进行关闭链接操作
      */
-    public static void closeConnection(){
+    public static void closeConnection() {
         Connection connection = CONNECTION_THREAD_LOCAL.get();
-        if(connection != null){
+        if (connection != null) {
             try {
                 connection.close();
             } catch (SQLException e) {
@@ -200,12 +309,13 @@ public class DatabaseHelper {
     /**
      * 此方法以及被后续的threadlocal 弃用
      * 关闭链接
+     *
      * @param connection 数据库链接
      * @deprecated 被新方法替代
      */
     @Deprecated
-    public static void closeConnection(Connection connection){
-        if(connection != null){
+    public static void closeConnection(Connection connection) {
+        if (connection != null) {
             try {
                 connection.close();
             } catch (SQLException e) {
